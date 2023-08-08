6789c2d2858845bd1550c7507319d5c87a6c9558
Soumish_Das <soumish.das@gmail.com>
2023-08-08 10:27:18 +0000

    Initial Commit

---
+++ lib/utils/MyRoutes.dart
@@ -0,0 +1,7 @@
+class MyRoutes {
+  static String initialPage = "/";
+  static String homepage = "/home";
+  static String loginpage = "/login";
+  static String profilepage = "/profile";
+  static String settings = "/profile/settings";
+}
